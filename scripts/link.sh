d48512c5bcac0dabe14f813e5276f9717823bbc6
ramroll <ramroll@gmail.com>
2021-07-28 13:45:11 +0800

    modify link

--- scripts/link.sh
+++ scripts/link.sh
@@ -4,9 +4,13 @@ cd "$DIR/../packages/skedo-request"
 yarn link
 cd "$DIR/../packages/skedo-core"
 yarn link
+cd "$DIR/../packages/skedo-render"
+yarn link @skedo/request
+yarn link @skedo/core
 cd "$DIR/../packages/skedo-ui"
 yarn link @skedo/core
 yarn link @skedo/request
+yarn link @skedo/skedo-render
 
 cd "$DIR/../packages/skedo-svc/config"
 yarn link
