324676e667e4a98e74ceabd2ff2252a05e8d319c
ramroll <ramroll@gmail.com>
2021-09-18 12:25:17 +0800

    修改链接

--- packages/skedo-request/src/config/config.prod.ts
+++ packages/skedo-request/src/config/config.prod.ts
@@ -3,23 +3,23 @@ import { ConfigAttributes } from "./ConfigAttributes";
 const config : ConfigAttributes = {
 	pageUrl: (name ? : string) => {
 		if(!name) {
-			return "https://api.skedo.cn/doc/page"
+			return "https://api.weavinghorse.com/doc/page"
 		}
-		return "https://api.skedo.cn/doc/page/" + name
+		return "https://api.weavinghorse.com/doc/page/" + name
 	},
-	uploadFileObject : "https://api.skedo.cn/upload/by-object",
-	uploadFileText : `https://api.skedo.cn/upload/by-content`,
+	uploadFileObject : "https://api.weavinghorse.com/upload/by-object",
+	uploadFileText : `https://api.weavinghorse.com/upload/by-content`,
 	componentUrl : (group? : string, name? : string) => {
 		if(!group || !name) {
-			return "https://api.skedo.cn/doc/component "
+			return "https://api.weavinghorse.com/doc/component "
 		}
-		return "https://api.skedo.cn/doc/component/" + group + "/" + name 
+		return "https://api.weavinghorse.com/doc/component/" + group + "/" + name 
 	},
 	codeProjectURL : (name : string) => {
-		return `https://api.skedo.cn/doc/code-project/${name}` 
+		return `https://api.weavinghorse.com/doc/code-project/${name}` 
 	},
 	codeProjectBuildURL : (name : string) => {
-		return `https://api.skedo.cn/runtime/build/${name}`
+		return `https://api.weavinghorse.com/runtime/build/${name}`
 	}
 }
 
