47935aa0f3041f6e16de1a78376c359adf22c76c
ramroll <ramroll@gmail.com>
2021-09-20 01:35:46 +0800

    build

--- packages/skedo-ui/config/webpack.config.js
+++ packages/skedo-ui/config/webpack.config.js
@@ -26,6 +26,7 @@ const ModuleNotFoundPlugin = require('react-dev-utils/ModuleNotFoundPlugin');
 const ForkTsCheckerWebpackPlugin = require('react-dev-utils/ForkTsCheckerWebpackPlugin');
 const typescriptFormatter = require('react-dev-utils/typescriptFormatter');
 const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const SpeedMeansurePlugin = require('speed-measure-webpack-plugin')
 
 const postcssNormalize = require('postcss-normalize');
 
@@ -576,6 +577,10 @@ module.exports = function (webpackEnv) {
     },
     plugins: [
       // Generates an `index.html` file with the <script> injected.
+      new webpack.ProvidePlugin({
+        React : 'react'
+      }),
+      new SpeedMeansurePlugin(),
       new HtmlWebpackPlugin(
         Object.assign(
           {},
